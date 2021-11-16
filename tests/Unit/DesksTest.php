35150c90ebfe4406683fd6650c202bd35c047eb6
ViktorVukevv <71508019+ViktorVukevv@users.noreply.github.com>
2021-11-16 22:48:32 +0200

    Unit testing

--- tests/Unit/DesksTest.php
+++ tests/Unit/DesksTest.php
@@ -27,4 +27,23 @@ class DesksTest extends TestCase
 
         $this->assertEquals(3.2, $desk->size);
     }
+    
+    public function testDeskPriceUpdating()
+    {
+        $desk = new Desk([
+            'is_taken' => 100,
+            'price' => 200.00,
+            'size' => 'medium',
+            'position' => 'next to the window'
+        ]);
+        $desk->save();
+
+        $desk = Desk::findOrFail($desk->id);
+
+        $desk->price = 150.00;
+
+        $desk->save();
+
+        $this->assertEquals(150, $desk->price);
+    }
 }
